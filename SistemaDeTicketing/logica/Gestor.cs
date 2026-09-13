d72ede88edb983d6ec486c7e16961849c3c5f904
ferna <sbarrientosa3@miumg.edu.gt>
2026-09-13 15:29:52 -0600

    creacion y modificacion de clases

--- SistemaDeTicketing/logica/Gestor.cs
+++ SistemaDeTicketing/logica/Gestor.cs
@@ -18,7 +18,20 @@ namespace SistemaDeTicketing.logica
 
         //Validaciones
         {
-            
+            if (string.IsNullOrEmpty(strNombre) || string.IsNullOrEmpty(strDescripcion) || string.IsNullOrEmpty(strCategoria) || string.IsNullOrEmpty(strPrioridad))
+            {
+                throw new ArgumentException("Todos los campos son obligatorios.");
+            }
+        }
+
+        ticket objTicket = new Ticket(strNombre, strDescripcion, intNumero, strCategoria, strPrioridad, objSolicitante);
+        objTicketTecnico(objTecnico);
+        //logica disponibilidadtecnico
+
+        publica ticket buscarticket(int intNumero)
+        {
+         ticket objticket = lstTicket.find(t => t.intNumero == intNumero);
+         return objticket;
         }
 
     }
